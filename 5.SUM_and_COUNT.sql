6e851c10a20fc1336c339bfaf8c47415a3ecf8b6
Wesley <matthew282@msn.com>
2022-05-06 14:01:04 -0700

    solutions

--- 5.SUM_and_COUNT.sql
+++ 5.SUM_and_COUNT.sql
@@ -1,41 +1,63 @@
--- 1.
+-- 1. Show the total population of the world.
 --
 
--- 2.
+SELECT SUM(population)
+FROM world
+
+
+-- 2. List all the continents - just once each.
 --
 
--- 3.
+SELECT DISTINCT continent
+FROM world;
+
+
+-- 3. Give the total GDP of Africa
 --
 
--- 4.
+SELECT SUM(gdp)
+FROM world 
+WHERE continent = 'Africa'
+GROUP BY continent;
+
+
+-- 4. How many countries have an area of at least 1000000
 --
 
--- 5.
+SELECT COUNT(name) AS count
+FROM world
+WHERE area >= 1000000;
+
+
+-- 5. What is the total population of ('Estonia', 'Latvia', 'Lithuania')
 --
 
--- 6.
+SELECT SUM(population)
+FROM world
+WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
+
+
+-- 6. For each continent show the continent and number of countries.
 --
 
--- 7.
+SELECT continent, COUNT(name)
+FROM world
+GROUP BY continent;
+
+
+-- 7. For each continent show the continent and number of countries with populations of at least 10 million.
 --
 
--- 8.
+SELECT continent, COUNT(name)
+FROM world
+WHERE population >= 10000000
+GROUP BY continent;
+
+
+-- 8. List the continents that have a total population of at least 100 million.
 --
 
--- 9.
---
-
--- 10.
---
-
--- 11.
---
-
--- 12.
---
-
--- 13.
---
-
--- 14.
---
+SELECT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) >= 100000000;
